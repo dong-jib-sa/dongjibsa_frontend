3083f2ad631d7bf7c9a2809d20a0e433121cfc56
Khyeji <khjji0502@gmail.com>
2023-07-27 16:49:42 +0900

    FEAT: TabBarViewController - 탭바 색상 변경 #3

--- DongJibSa/DongJibSa/Controller/TabBarViewController.swift
+++ DongJibSa/DongJibSa/Controller/TabBarViewController.swift
@@ -12,17 +12,7 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        UITabBar.appearance().tintColor = .primaryColor
+        UITabBar.appearance().unselectedItemTintColor = .bodyColor
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
