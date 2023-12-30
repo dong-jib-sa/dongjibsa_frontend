949838279840022984e4ef4ec31ad7e42ed90fa0
Khyeji <khjji0502@gmail.com>
2023-12-30 13:50:07 +0900

    MODI: 로그인 화면에서 둘러보기 기능 블라인드 처리

--- DongJibSa/DongJibSa/Controller/Login/LoginViewController.swift
+++ DongJibSa/DongJibSa/Controller/Login/LoginViewController.swift
@@ -14,7 +14,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setNavigationBar()
+//        setNavigationBar()
         setupView()
     }
     
