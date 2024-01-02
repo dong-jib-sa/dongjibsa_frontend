e6ea3b4af0ad04940f10433d4022b2378666c6fc
Khyeji <khjji0502@gmail.com>
2024-01-02 17:04:27 +0900

    FEAT: 애플 로그인 환경 세팅

--- DongJibSa/DongJibSa/Controller/Login/LoginViewController.swift
+++ DongJibSa/DongJibSa/Controller/Login/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class LoginViewController: UIViewController {
     
@@ -45,6 +46,7 @@ class LoginViewController: UIViewController {
             make.edges.equalToSuperview()
         }
         
+        loginView.appleLoginButton.addTarget(self, action: #selector(appleLoginButtonTapped), for: .touchUpInside)
         loginView.phoneLoginButton.addTarget(self, action: #selector(phoneLoginButtonTapped), for: .touchUpInside)
     }
     
@@ -55,8 +57,51 @@ class LoginViewController: UIViewController {
         self.navigationController?.pushViewController(viewController, animated: true)
     }
     
+    @objc func appleLoginButtonTapped(_ sender: UIButton) {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.email, .fullName]
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
     @objc func phoneLoginButtonTapped(_ sender: UIButton) {
         let viewController = PhoneCertifyViewController()
         self.navigationController?.pushViewController(viewController, animated: true)
+    }
+}
+
+extension LoginViewController: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    // 로그인을 진행하는 화면
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+        
+    // 로그인 성공
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential {
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            let idToken = appleIDCredential.identityToken!
+            let tokeStr = String(data: idToken, encoding: .utf8)
+            
+            print("User ID : \(userIdentifier)")
+            print("User Email : \(email ?? "")")
+            print("User Name : \((fullName?.givenName ?? "") + (fullName?.familyName ?? ""))")
+            print("token : \(String(describing: tokeStr))")
+            // MEMO: 화면 전환
+            
+        default:
+            break
+        }
+    }
+    
+    // 로그인 실패
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print(error.localizedDescription)
     }
 }
