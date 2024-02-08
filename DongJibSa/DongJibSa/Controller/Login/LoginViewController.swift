e7042c139a7efa6b40a4bd636b80c789c59fba4b
Khyeji <khjji0502@gmail.com>
2024-02-08 16:28:14 +0900

    MODI: 둘러보기 기능 수정

--- DongJibSa/DongJibSa/Controller/Login/LoginViewController.swift
+++ DongJibSa/DongJibSa/Controller/Login/LoginViewController.swift
@@ -18,30 +18,30 @@ final class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        setNavigationBar()
+        setNavigationBar()
         setupView()
     }
     
-//    private func setNavigationBar() {
-//        var attString = AttributedString("둘러보기")
-//        attString.font = .systemFont(ofSize: 12, weight: .regular)
-//        attString.foregroundColor = .systemGray
-//        
-//        var configuration = UIButton.Configuration.plain()
-//        configuration.attributedTitle = attString
-//        configuration.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
-//        configuration.imagePlacement = .trailing
-//        configuration.imagePadding = 6
-//        
-//        let skipButton = UIButton(configuration: configuration)
-//        skipButton.setImage(UIImage(systemName: "chevron.forward"), for: .normal)
-//        skipButton.setPreferredSymbolConfiguration(.init(pointSize: 12), forImageIn: .normal)
-//        skipButton.tintColor = .systemGray
-//        skipButton.addTarget(self, action: #selector(skipButtonTapped), for: .touchUpInside)
-//        
-//        let skipItem = UIBarButtonItem(customView: skipButton)
-//        self.navigationItem.rightBarButtonItem = skipItem
-//    }
+    private func setNavigationBar() {
+        var attString = AttributedString("둘러보기")
+        attString.font = .systemFont(ofSize: 12, weight: .regular)
+        attString.foregroundColor = .systemGray
+        
+        var configuration = UIButton.Configuration.plain()
+        configuration.attributedTitle = attString
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        configuration.imagePlacement = .trailing
+        configuration.imagePadding = 6
+        
+        let skipButton = UIButton(configuration: configuration)
+        skipButton.setImage(UIImage(systemName: "chevron.forward"), for: .normal)
+        skipButton.setPreferredSymbolConfiguration(.init(pointSize: 12), forImageIn: .normal)
+        skipButton.tintColor = .systemGray
+        skipButton.addTarget(self, action: #selector(skipButtonTapped), for: .touchUpInside)
+        
+        let skipItem = UIBarButtonItem(customView: skipButton)
+        self.navigationItem.rightBarButtonItem = skipItem
+    }
     
     private func setupView() {
         self.view.addSubview(loginView)
@@ -53,12 +53,12 @@ final class LoginViewController: UIViewController {
         loginView.phoneLoginButton.addTarget(self, action: #selector(phoneLoginButtonTapped), for: .touchUpInside)
     }
     
-//    @objc func skipButtonTapped(_ sender: UIButton) {
-//        let viewController = LocationSettingViewController(selectLocation: [])
-//        self.navigationController?.navigationBar.tintColor = .bodyColor
-//        self.navigationItem.backButtonDisplayMode = .minimal
-//        self.navigationController?.pushViewController(viewController, animated: true)
-//    }
+    @objc func skipButtonTapped(_ sender: UIButton) {
+        let main = UIStoryboard.init(name: "Main", bundle: nil)
+        let viewController = main.instantiateViewController(identifier: "TabBarViewController") as! TabBarViewController
+        viewController.modalPresentationStyle = .fullScreen
+        self.present(viewController, animated: false)
+    }
     
     // KAKAO SNS Login
     @objc private func kakaoLoginButtonTapped(_ sender: UIButton) {
